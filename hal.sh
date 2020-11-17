f5bdca3dec6ac3061eb590801209d2ab175f92ae
Apon77 <khalakuzzamanapon5@gmail.com>
2020-11-17 10:49:55 +0600

    Add los hal

---
+++ hal.sh
@@ -0,0 +1,1 @@
+cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8996 media && gcl https://github.com/LineageOS/android_hardware_qcom_audio/ -b lineage-18.0-caf-msm8996 audio && gcl https://github.com/LineageOS/android_hardware_qcom_display/ -b lineage-18.0-caf-msm8996 display
