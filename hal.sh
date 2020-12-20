2898a190769e6c8c2eeee4568b90aa4cbe55a442
Apon77 <khalakuzzamanapon5@gmail.com>
2020-12-20 05:20:05 +0000

    18.1

--- hal.sh
+++ hal.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8996 media && gcl https://github.com/LineageOS/android_hardware_qcom_audio/ -b lineage-18.0-caf-msm8996 audio && gcl https://github.com/LineageOS/android_hardware_qcom_display/ -b lineage-18.0-caf-msm8996 display && cd -
+cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 media && gcl https://github.com/LineageOS/android_hardware_qcom_audio/ -b lineage-18.1-caf-msm8996 audio && gcl https://github.com/LineageOS/android_hardware_qcom_display/ -b lineage-18.1-caf-msm8996 display && cd -
