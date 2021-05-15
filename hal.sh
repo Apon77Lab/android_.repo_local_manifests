46eb44908c056eebee44711f01a57a46af063716
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-16 01:43:01 +0600

    remove unnecessary things

--- hal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-alias gcl='git clone'
-
-cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/android-eleven/hardware_qcom_media -b caf-msm8996-r media && gcl https://github.com/android-eleven/hardware_qcom_display -b caf-msm8996-r display && gcl https://github.com/android-eleven/hardware_qcom_audio -b caf-msm8996-r audio && cd -
